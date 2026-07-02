e46c99012b92055d2911657982a1fa149a18920e
inrain9953 <inrainconstruction46@gmail.com>
2026-07-02 16:55:13 +0530

    Config file refactor

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  images: {
-    formats: ['image/avif', 'image/webp']
-  },
   /* config options here */
   reactCompiler: true
 }
