3f3a4ff5919656e7cf8a0964aa43c616cc321fd0
inrain9953 <inrainconstruction46@gmail.com>
2026-07-07 10:41:54 +0530

    Navbar modification

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -242,7 +242,7 @@ export const Navbar = () => {
       </nav>
 
       {/* ── Mobile Bottom Nav ── */}
-      <nav className='md:hidden fixed bottom-0 left-0 right-0 h-16 bg-white border-t border-gray-200 shadow-[0_-2px_16px_rgba(0,0,0,0.08)] z-50 px-2'>
+      <div className='md:hidden fixed bottom-0 left-0 right-0 h-16 bg-white border-t border-gray-200 shadow-[0_-2px_16px_rgba(0,0,0,0.08)] z-50 px-2'>
         <div className='flex justify-around items-center h-full'>
           {BOTTOM_TABS.map(({ label, icon, href }) => (
             <button
@@ -264,7 +264,7 @@ export const Navbar = () => {
             </button>
           ))}
         </div>
-      </nav>
+      </div>
 
       {/* ── Sidebar Overlay ── */}
       {sidebarOpen && (
